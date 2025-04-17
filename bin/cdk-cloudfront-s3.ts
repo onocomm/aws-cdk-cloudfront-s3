cd20c8f32e454de558992a97e2d4438f6a921588
onoyamayoichi <yonoyama@onocomm.co.jp>
2025-04-17 14:44:46 +0900

    CdkCloudFrontS3Stackの引数をバッククオートからシングルクオートに変更

--- bin/cdk-cloudfront-s3.ts
+++ bin/cdk-cloudfront-s3.ts
@@ -4,7 +4,7 @@ import { CdkCloudFrontS3Stack } from '../lib/cdk-cloudfront-s3-stack';
 
 const app = new cdk.App();
 
-new CdkCloudFrontS3Stack(app, `CdkCloudFrontS3Stack`, {
+new CdkCloudFrontS3Stack(app, 'CdkCloudFrontS3Stack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: 'us-east-1',
